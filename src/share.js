6a8b44c38cbe5402fed755ec8fb9b82a58760c88
jiun Kim <lrnngman@gmail.com>
2021-10-20 21:39:58 +1100

    test branch

--- src/share.js
+++ src/share.js
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet-async";
 export const HelmetTitle = ({ title }) => {
   return (
     <Helmet>
-      <title>{title} | JIUN STUDIO</title>
+      <title>{title} | JIUN STUDIO.</title>
     </Helmet>
   );
 };
