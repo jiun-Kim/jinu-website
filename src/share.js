faf3e41de27d72cb62898438122e2481169acab7
Jiun <lrnngman@gmail.com>
2021-10-20 21:31:30 +1100

    Title name changed

--- src/share.js
+++ src/share.js
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet-async";
 export const HelmetTitle = ({ title }) => {
   return (
     <Helmet>
-      <title>{title} | JINU STUDIO</title>
+      <title>{title} | JIUN STUDIO</title>
     </Helmet>
   );
 };
